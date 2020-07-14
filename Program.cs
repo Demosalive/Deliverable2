70df57cda92bbd766dc92e9015b557941d3cd1d9
Curtis Godzisz <curtisgodzisz@gmail.com>
2020-07-14 19:29:55 -0400

    Second assignment for Unit 1

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+
+namespace Deliverable2
+{ 
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your message?");
+
+            var input = Console.ReadLine();
+            var message = "";
+            var count = 0;
+            var checkSum = 0;
+            
+            foreach (char c in input.ToUpper())
+            {
+                message = message + ((int)c - 64);
+                count++;
+                if (input.Length > count)
+                    message = (message + "-");
+
+                checkSum = checkSum + c;
+            }
+         
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " +checkSum);
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
